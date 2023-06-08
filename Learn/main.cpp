4741a70eb15053b00aede0f15c83c1c573f8b23f
Radixactivetea <115629911+Radixactivetea@users.noreply.github.com>
2023-06-08 08:37:36 +0800

    Add files via upload

---
+++ Learn/main.cpp
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include <GL/glut.h>
+#include <iostream>
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+
+    int width = 400;
+    int height = 400;
+    glutInitWindowSize(width, height);
+
+    int x = 200;
+    int y = 100;
+    glutInitWindowPosition(x, y);
+    int win = glutCreateWindow("Tutorial 01");
+    printf("window id: %d\n", win);
+
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glutMainLoop();
+
+    return 0;
+}
